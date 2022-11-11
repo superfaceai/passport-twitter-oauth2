c363a6cde7247a0ef09bbffcb1b47a013bbb5724
Jan Halama <mail@janhalama.cz>
2022-11-11 09:36:23 +0100

    fix: make authorizationURL and tokenURL strategy options optional

--- src/models/strategyOptions.ts
+++ src/models/strategyOptions.ts
@@ -1,20 +1,28 @@
-import { StrategyOptions, StrategyOptionsWithRequest } from 'passport-oauth2';
+import {
+  StrategyOptions as PassportOAuth2StrategyOptions,
+  StrategyOptionsWithRequest as PassportOAuth2StrategyOptionsWithRequest,
+} from 'passport-oauth2';
 
 interface TwitterStrategyOptionsBase {
   clientType: 'public' | 'confidential' | 'private'; // OAuth 2.0 client types as defined here: https://datatracker.ietf.org/doc/html/rfc6749#section-2.1 and configured in Twitter developer portal
   clientID: string;
   clientSecret: string;
-  userProfileUrl?: string;
+  userProfileUrl?: string | undefined;
+  authorizationURL?: string | undefined;
+  tokenURL?: string | undefined;
 }
 
 export interface StrategyOption
   extends TwitterStrategyOptionsBase,
-    StrategyOptions {
+    Omit<PassportOAuth2StrategyOptions, 'authorizationURL' | 'tokenURL'> {
   passReqToCallback?: false | undefined;
 }
 
 export interface StrategyOptionWithRequest
   extends TwitterStrategyOptionsBase,
-    StrategyOptionsWithRequest {
+    Omit<
+      PassportOAuth2StrategyOptionsWithRequest,
+      'authorizationURL' | 'tokenURL'
+    > {
   passReqToCallback: true;
 }
