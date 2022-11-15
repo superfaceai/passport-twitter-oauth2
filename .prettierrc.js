68d3d3cabd648cda5856992c8ef46e942f05e0ec
Jan Vlnas <jan.vlnas@superface.ai>
2022-11-15 10:04:00 +0100

    chore(editorconfig): Add & apply editorconfig
    Also remove duplicate rules from .prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,8 @@
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
   semi: true,
   singleQuote: true,
-  quoteProps: "as-needed",
-  trailingComma: "es5",
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
   bracketSpacing: true,
-  arrowParens: "avoid",
-  endOfLine: "lf",
+  arrowParens: 'avoid',
 };
