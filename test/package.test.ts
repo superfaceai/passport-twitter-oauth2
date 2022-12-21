a0231451219cc3cfeb1eb0ee25ca74d3b6f68f94
Jan Halama <mail@janhalama.cz>
2022-12-21 08:33:32 +0100

    test: import in TypeScript

---
+++ test/package.test.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+
+import StrategyDefaultImport, { Strategy as StrategyNamedImport } from '..';
+
+describe('@superfaceai/passport-twitter-oauth2 package', function () {
+  describe('when using in TypeScript project', () => {
+    it('should be able to import named Strategy constructor', function () {
+      expect(StrategyNamedImport).to.be.a('function');
+    });
+
+    it('should able to import default Strategy constructor', function () {
+      expect(StrategyDefaultImport).to.be.a('function');
+      expect(StrategyDefaultImport).to.equal(StrategyNamedImport);
+    });
+  });
+});
