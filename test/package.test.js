72bb496618d1f3b3da55d1b8fd9ae4d77be6dd02
Jan Halama <mail@janhalama.cz>
2022-12-21 08:34:54 +0100

    test: update description of CommonJS package require tests

--- test/package.test.js
+++ test/package.test.js
@@ -4,13 +4,15 @@
 const { Strategy: StrategyNamedImport } = require('..');
 const StrategyDefaultImport = require('..');
 
-describe('@superfaceai/passport-twitter-oauth2', function () {
-  it('should export named Strategy constructor', function () {
-    expect(StrategyNamedImport).to.be.a('function');
-  });
+describe('@superfaceai/passport-twitter-oauth2 package', function () {
+  describe('when using in CommonJS project', () => {
+    it('should be able to require named Strategy constructor', function () {
+      expect(StrategyNamedImport).to.be.a('function');
+    });
 
-  it('should export default Strategy constructor', function () {
-    expect(StrategyDefaultImport).to.be.a('function');
-    expect(StrategyDefaultImport).to.equal(StrategyNamedImport);
+    it('should be able to require default Strategy constructor', function () {
+      expect(StrategyDefaultImport).to.be.a('function');
+      expect(StrategyDefaultImport).to.equal(StrategyNamedImport);
+    });
   });
 });
